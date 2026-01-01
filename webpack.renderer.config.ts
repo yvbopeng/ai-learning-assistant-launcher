60af6d460bea161820c82d66cc158ca4befe6619
James Zhang <shiftonetothree@126.com>
2026-01-01 08:20:55 +0000

    chore(webpack.renderer.config.ts): remove useless code

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,9 +1,6 @@
 import type { Configuration } from 'webpack';
-import webpack from 'webpack';
-
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
-import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
 
 rules.push();
 
@@ -11,45 +8,8 @@ export const rendererConfig: Configuration = {
   module: {
     rules,
   },
-  plugins: [
-    // This line is about WebTorrent in Browser configuration
-    new NodePolyfillPlugin({
-      additionalAliases: [
-        'path',
-        'crypto',
-        'fs',
-        'http',
-        'os',
-      ]
-    }),
-    // This line is about WebTorrent in Browser configuration
-    new webpack.ProvidePlugin({
-      process: '/process-fast.js',
-    }),
-    // This line is about WebTorrent in Browser configuration
-    new webpack.DefinePlugin({
-      global: 'globalThis',
-    }),
-    ...plugins,
-  ],
+  plugins: [...plugins],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.scss', '.svg'],
-    // aliasFields: ['browser'],
-    alias: {
-      // Below is about WebTorrent in Browser configuration
-      // path: 'path-esm',
-      './lib/conn-pool.js': false,
-      './lib/utp.cjs': false,
-      '@silentbot1/nat-api': false,
-      'bittorrent-dht': false,
-      // crypto: false,
-      // fs: false,
-      // 'fs-chunk-store': 'fsa-chunk-store',
-      // http: false,
-      'load-ip-set': false,
-      net: false,
-      // os: false,
-      ut_pex: false,
-    },
   },
 };
